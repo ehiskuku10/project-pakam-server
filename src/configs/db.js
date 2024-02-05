d52519051c5fae4a8e8325a321c738781e93625d
Caleb <ehiskuku@gmail.com>
2024-02-05 09:20:02 +0100

    Ready for deploy

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,8 +1,16 @@
 const Pool = require('pg').Pool
 
 exports.pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  port: 5432, 
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 })
+
+// exports.pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   port: 5432,
+// })
