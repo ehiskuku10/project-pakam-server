ebffaf53078915812a4f7c874138ebf79341fab1
Caleb <ehiskuku@gmail.com>
2024-02-05 11:14:36 +0100

    Ready for deploy

--- src/configs/db.js
+++ src/configs/db.js
@@ -7,13 +7,3 @@ exports.pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
 })
-
-// exports.pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'postgres',
-//   port: 5432,
-// })
-
-// https://project-pakam.vercel.app
-// https://pakam-caa42362631b.herokuapp.com
