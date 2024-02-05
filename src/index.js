0ae2699c9596a90a9cd9ab0f89e5dcac2b1b63e1
Caleb <ehiskuku@gmail.com>
2024-02-05 10:03:05 +0100

    Ready for deploy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
-const port = 3002
+const port = process.env.PORT || 3002
 
 app.use(cors())
 app.use(bodyParser.json())
