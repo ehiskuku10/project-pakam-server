d543ff7bcdead2367fd8e4686eb0bdd7b8aef5d7
Caleb <ehiskuku@gmail.com>
2024-02-06 08:17:01 +0100

    One and only commit

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -16,6 +16,7 @@ exports.isLoggedIn = (request, response, next) => {
       data: { isLoggedIn: 0 },
     })
   }catch(error) {
+    console.log('HAPPENED HERE');
     response.status(500).send({
       status: 'failed',
       message: `Something we didn't expect has happened!`
